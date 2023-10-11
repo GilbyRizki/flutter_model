6bd19add5bf81bd674c8604286a40f16f1204fe9
GilbyRizki <112262179+GilbyRizki@users.noreply.github.com>
2023-10-11 08:06:51 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:new_project_flutter/screen_satu.dart';
-import 'package:new_project_flutter/screen_dua.dart';
+import 'package:oktober9/postView.dart';
+import 'home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,19 +10,31 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      initialRoute: '/',
       getPages: [
-        GetPage(name: "/", page: ()=>MyApp()),
-        GetPage(name: "/screenSatu", page: ()=>ScreenSatu()),
-        GetPage(name: "/screenDua", page: ()=>ScreenDua()),
+        GetPage(name: '/', page: () => MyApp()),
+        GetPage(name: '/home', page: () => home()),
+        GetPage(name: '/postview', page: () => postView()),
       ],
+      title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
       ),
-      home:  ScreenSatu(),
+      home: home(),
     );
   }
 }
